d6e3b6d1cfbe1521760826c71b6add48539ab05d
ryan77710 <lolliaryan@ryan-ordi.local>
2021-10-19 18:43:03 +0200

    add friend and profile css

--- src/pages/FriendPage/FriendPage.js
+++ src/pages/FriendPage/FriendPage.js
@@ -1,13 +1,75 @@
+import { useState, useEffect } from "react";
 import { Redirect } from "react-router-dom";
 
 const FriendPage = (props) => {
   const { authToken } = props;
+  const [invitationSelected, setInvitationSelected] = useState(false);
+  const [friendSearch, setFriendSearch] = useState("");
+
+  const handleFriendSearchChange = (event) => {
+    setFriendSearch(event.target.value);
+  };
   return (
     <div>
       {authToken ? (
         <div className="friend-page">
-          <h1>friends</h1>
-          <div>friends list</div>
+          <h1>
+            <span onClick={() => setInvitationSelected(false)} className={!invitationSelected ? "title-selected" : ""}>
+              friends(2)
+            </span>
+            <img src="/mielo1.png" alt="mielo" />
+            <span onClick={() => setInvitationSelected(true)} className={invitationSelected ? "title-selected" : ""}>
+              invitations(1)
+            </span>
+          </h1>
+          <div>
+            <div className="filter">
+              <div>
+                <div className="search">
+                  <p>Name :</p>
+                  <input type="text" onChange={handleFriendSearchChange} value={friendSearch} />
+                </div>
+                <div className="select-gender">
+                  <label for="gender">Gender:</label>
+                  <select name="gender" id="gender">
+                    <option value="">-- choose a gender--</option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="other">Other</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            {invitationSelected ? (
+              <div className="friend-request-container">
+                <div className="friend-request">
+                  <img src="/anonymous.jpeg" alt="username" />
+                  <p title="username">username</p>
+                  <button className="friend-request-post">posts</button>
+                  <button className="friend-request-picture">pictures</button>
+                  <button className="friend-request-message">add</button>
+                  <button className="friend-request-delete">refuse</button>
+                </div>
+              </div>
+            ) : (
+              <div className="friend-container">
+                <div className="friend">
+                  <img src="/anonymous.jpeg" alt="username" /> <p title="username">username</p>
+                  <button className="friend-post">posts</button>
+                  <button className="friend-picture">pictures</button>
+                  <button className="friend-message">messages</button>
+                  <button className="friend-delete">X</button>
+                </div>
+                <div className="friend">
+                  <img src="/anonymous.jpeg" alt="username" /> <p title="username">username</p>
+                  <button className="friend-post">posts</button>
+                  <button className="friend-picture">pictures</button>
+                  <button className="friend-message">messages</button>
+                  <button className="friend-delete">X</button>
+                </div>
+              </div>
+            )}
+          </div>
         </div>
       ) : (
         <Redirect to="/" />
--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -1,23 +1,112 @@
 import { Redirect } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+import axios from "axios";
 
 const ProfilePage = (props) => {
   const { authToken } = props;
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(`${process.env.REACT_APP_API_URL}user-profile/${authToken}`);
+      setData(response.data);
+      setIsLoading(false);
+    };
+    fetchData();
+  }, [authToken]);
   return (
     <div>
       {authToken ? (
         <div className="my-profile-page">
           <h1>my profile</h1>
-          <div>
-            photo + username + description + button mettre a jour le profile
-            <br />
-            photo du jour + note
-            <br />
-            photos
-            <br />
-            posts
-            <br />
-            about
-          </div>
+          {isLoading ? (
+            <div>is loading</div>
+          ) : (
+            <div className="user-container">
+              <div className="user-basic">
+                <div className="user-info-1">
+                  <img src={data.user.profilePicture.secure_url} alt="user-profile" /> <p>username</p>
+                </div>
+                <p className="description">{data.user.description}</p>
+                <div className="user-info-2">
+                  <p>rate:16/20</p>
+                  <img src={data.user.profilePicture.secure_url} alt="pic-day" />
+                </div>
+                <div className="user-basic-button">
+                  <button>update your profile</button>
+                </div>
+              </div>
+              <div className="about-container">
+                <div className="links">
+                  <div>
+                    <h2>personal links</h2> <button>+</button>
+                  </div>
+
+                  <div className="links-container">
+                    <div className="link">
+                      <img src={"/link-picture/you-tube.jpeg"} alt="link" />
+                    </div>
+                    <div className="link">
+                      <img src={"/link-picture/facebook.png"} alt="link" />
+                    </div>
+                    <div className="link">
+                      <img src={"/link-picture/instagram.jpg"} alt="link" />
+                    </div>
+                    <div className="link">
+                      <img src={"/link-picture/linkedin.png"} alt="link" />
+                    </div>
+                    <div className="link">
+                      <img src={"/link-picture/snap.png"} alt="link" />
+                    </div>
+                    <div className="link">
+                      <img src={"/link-picture/tiktok.png"} alt="link" />
+                    </div>
+                    <div className="link">
+                      <img src={"/link-picture/twitch.png"} alt="link" />
+                    </div>
+                    <div className="link">
+                      <img src={"/link-picture/twitter.png"} alt="link" />
+                    </div>
+                    <div className="link">
+                      <img src={"/link-picture/whatsapp.jpg"} alt="link" />
+                    </div>
+                  </div>
+                </div>
+                <div className="series">
+                  <div>
+                    <h2>top series</h2>
+                    <button>+</button>
+                  </div>
+                  <div className="series-container">
+                    <div className="serie">
+                      <span>1</span> <h3>title</h3>
+                    </div>
+                  </div>
+                </div>
+                <div className="movies">
+                  <div>
+                    <h2>top movies</h2>
+                    <button>+</button>
+                  </div>
+                </div>
+                <div className="musics">
+                  <div>
+                    <h2>top musics</h2>
+                    <button>+</button>
+                  </div>
+                </div>
+                <div className="mangas">
+                  <div>
+                    <h2>top manga</h2>
+                    <button>+</button>
+                  </div>
+                </div>
+              </div>
+              <div>posts</div>
+            </div>
+          )}
         </div>
       ) : (
         <Redirect to="/" />
